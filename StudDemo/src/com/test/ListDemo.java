9d584230a91d75ea5149af177baff3be7295bcce
HP <HP@shree>
2021-06-27 10:53:12 +0530

    list object added

--- StudDemo/src/com/test/ListDemo.java
+++ StudDemo/src/com/test/ListDemo.java
@@ -1,7 +1,13 @@
 package com.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListDemo {
 	public static void main(String[] args) {
+		List<String> ll= new ArrayList<String>();
+		
+		
 		
 	}
 
